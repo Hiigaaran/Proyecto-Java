6a4b4974aef6ef7835436d0db4f048670a67be19
jordansilva1990 <jordan.silva@hotmail.es>
2015-11-17 17:45:44 -0300

    SQL funcional+web-inf

--- puntoventa.sql
+++ puntoventa.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-11-2015 a las 02:45:00
+-- Tiempo de generación: 17-11-2015 a las 21:37:16
 -- Versión del servidor: 5.6.26
 -- Versión de PHP: 5.6.12
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `puntoventa`
 --
+CREATE DATABASE IF NOT EXISTS `puntoventa` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `puntoventa`;
 
 -- --------------------------------------------------------
 
@@ -54,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `detalle_venta` (
 --
 
 CREATE TABLE IF NOT EXISTS `producto` (
-  `cod_producto` varchar(10) NOT NULL,
+  `cod_producto` int(10) NOT NULL,
   `nombre` varchar(15) NOT NULL,
   `clase` varchar(15) NOT NULL,
   `descripcion` varchar(30) NOT NULL,
@@ -102,7 +104,9 @@ ALTER TABLE `cliente`
 -- Indices de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
-  ADD PRIMARY KEY (`cod_detalle`);
+  ADD PRIMARY KEY (`cod_detalle`),
+  ADD KEY `cod_venta` (`cod_venta`),
+  ADD KEY `cod_producto` (`cod_producto`);
 
 --
 -- Indices de la tabla `producto`
@@ -120,7 +124,46 @@ ALTER TABLE `vendedor`
 -- Indices de la tabla `venta`
 --
 ALTER TABLE `venta`
-  ADD PRIMARY KEY (`cod_venta`);
+  ADD PRIMARY KEY (`cod_venta`),
+  ADD KEY `rut_cliente` (`rut_cliente`),
+  ADD KEY `rut_vendedor` (`rut_vendedor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `cod_detalle` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `cod_producto` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `cod_venta` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`cod_venta`) REFERENCES `venta` (`cod_venta`),
+  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`cod_producto`) REFERENCES `producto` (`cod_producto`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`rut_cliente`) REFERENCES `cliente` (`rut_cliente`),
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`rut_vendedor`) REFERENCES `vendedor` (`rut_vendedor`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
