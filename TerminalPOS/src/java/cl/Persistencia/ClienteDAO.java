65979f636a02aee6d6f7daeee9092b0d7560e874
Sergio Matamala <smatamala92@gmail.com>
2015-11-20 17:00:06 -0300

    Avance DAO

--- TerminalPOS/src/java/cl/Persistencia/ClienteDAO.java
+++ TerminalPOS/src/java/cl/Persistencia/ClienteDAO.java
@@ -1,4 +1,3 @@
-
 package cl.Persistencia;
 
 import cl.dominio.Cliente;
@@ -8,6 +7,55 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ClienteDAO {
-    
-    
+
+    private Connection cnx;
+
+    public ClienteDAO(Connection cnx) {
+
+        this.cnx = cnx;
+
+    }
+
+    public void agregar(Cliente c) {
+
+        String sql = "insert into cliente "
+                + "(rut_cliente,nombre,apellido,direccion,correo)"
+                + "values (?,?,?,?,?)";
+
+        try (PreparedStatement stmt = cnx.prepareStatement(sql)) {
+            stmt.setString(1, c.getRutCliente());
+            stmt.setString(2, c.getNombre());
+            stmt.setString(3, c.getApellido());
+            stmt.setString(4, c.getDireccion());
+            stmt.setString(5, c.getCorreo());
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al Agregar Cliente");
+        }
+    }
+
+    public Cliente buscarPorRutCliente(String rutCliente) {
+
+        String sql = "select * from Cliente where rut_cliente = ?";
+        Cliente c = null;
+
+        try (PreparedStatement stmt = cnx.prepareStatement(sql))
+        {
+            stmt.setString(1, rutCliente);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    c.setRutCliente(rs.getString("rut_cliente"));
+                    c.setNombre(rs.getString("nombre"));
+                    c.setApellido(rs.getString("apellido"));
+                    c.setDireccion(rs.getString("direccion"));
+                    c.setCorreo(rs.getString("correo"));
+                }
+
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error al Buscar Peticion");
+        }
+        return c;
+    }
+
 }
