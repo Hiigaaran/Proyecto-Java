38c0ee86a55e633e7da52730b7ed21dfd1156d67
Sergio Matamala <smatamala92@gmail.com>
2015-11-20 16:10:20 -0300

    Persistencia

---
+++ TerminalPOS/src/java/cl/Persistencia/ClienteDAO.java
@@ -0,0 +1,13 @@
+
+package cl.Persistencia;
+
+import cl.dominio.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClienteDAO {
+    
+    
+}
